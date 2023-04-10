cbb7d75816f4bbb42f5f7753d3b042ea6040edbe
Ersan Ceylan <ersanceylan@Ersans-MacBook-Air-2.local>
2023-04-10 17:56:06 +0300

    vira bismillah

---
+++ src/main/java/online/lokals/lokalapi/LokalApiApplication.java
@@ -0,0 +1,13 @@
+package online.lokals.lokalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LokalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/lokals/lokalapi/LokalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package online.lokals.lokalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
